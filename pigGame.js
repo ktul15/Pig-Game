5c4bd01a318fb4db046fe351b612c1ed734a8ea1
Ketul Makwana <ketulmakwana09@gmail.com>
2019-03-06 23:51:30 +0530

    Added functionalities for another dice in javascript.

--- pigGame.js
+++ pigGame.js
@@ -1,9 +1,11 @@
 var rollDice = document.querySelector(".btn-roll");
 var holdDice = document.querySelector(".btn-hold");
 var newGame = document.querySelector(".btn-new");
-var image = document.querySelector(".dice");
+var dice_1 = document.querySelector(".dice-1");
+var dice_2 = document.querySelector(".dice-2");
 var winScore = document.querySelector("#winScore");
-var diceNum, scores, currentScore, activePlayer, gamePlaying, previousDice = 0;
+var diceNum1, diceNum2, scores, currentScore, activePlayer, gamePlaying, previousDice1 = 0,
+    previousDice2 = 0;
 
 init();
 
@@ -13,22 +15,25 @@ rollDice.addEventListener("click", function() {
         // 1. Generate a random number and save it in a variable called "diceNum".
         generateRandomNum();
 
-        console.log(diceNum, previousDice);
+        console.log(diceNum1, diceNum2, previousDice1, previousDice2);
         // 2. If diceNum is not 1 then add the diceNum to currentScore otherwise go to next player.
-        if (diceNum !== 1 && diceNum !== previousDice) {
-            // Display the image
-            image.style.display = "block";
+        if ((diceNum1 > 1 && diceNum2 > 1) && (diceNum1 !== previousDice1 && diceNum2 !== previousDice2)) {
+            // Display the dice-1
+            dice_1.style.display = "block";
+            dice_2.style.display = "block";
 
-            // Display the image corresponding to diceNum
-            image.src = "dice-" + diceNum + ".png";
+            // Display the dice-1 corresponding to diceNum
+            dice_1.src = "dice-" + diceNum1 + ".png";
+            dice_2.src = "dice-" + diceNum2 + ".png";
 
             //add diceNum to currentScore.
-            currentScore += diceNum;
+            currentScore += diceNum1;
+            currentScore += diceNum2;
 
             // Display the currentScore.
             document.querySelector("#current-" + activePlayer).textContent = currentScore;
         } else {
-            if (diceNum === 6) {
+            if (diceNum1 === 6 && diceNum2 === 6) {
                 scores[activePlayer] = 0;
                 document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
             }
@@ -37,11 +42,13 @@ rollDice.addEventListener("click", function() {
 
         // 3. if a player rolls two 6 in a row then call the nextPlayer() function..
         // Store the diceNum in another variable if it is 6.
-        diceNum === 6 ? previousDice = diceNum : previousDice = 0;
-        console.log(diceNum, previousDice);
+        diceNum1 === 6 ? previousDice1 = diceNum1 : previousDice1 = 0;
+        diceNum2 === 6 ? previousDice2 = diceNum2 : previousDice2 = 0;
+
+        console.log(diceNum1, diceNum2, previousDice1, previousDice2);
+
     }
 });
-
 
 // hold the score Event
 holdDice.addEventListener("click", function() {
@@ -54,7 +61,8 @@ holdDice.addEventListener("click", function() {
         // Chack if player won the game.
         if (scores[activePlayer] >= winScore.value) {
             document.querySelector("#name-" + activePlayer).textContent = "Winner!";
-            document.querySelector(".dice").style.display = "none";
+            document.querySelector(".dice-1").style.display = "none";
+            document.querySelector(".dice-2").style.display = "none";
             document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
             gamePlaying = false;
         } else {
@@ -69,7 +77,8 @@ newGame.addEventListener("click", init)
 
 //generate a random number and save it into a "diceNum" variable;
 function generateRandomNum() {
-    diceNum = Math.floor(Math.random() * 6 + 1);
+    diceNum1 = Math.floor(Math.random() * 6 + 1);
+    diceNum2 = Math.floor(Math.random() * 6 + 1);
 }
 
 // game initializer function
@@ -84,7 +93,8 @@ function init() {
     document.getElementById("current-0").textContent = "0";
     document.getElementById("current-1").textContent = "0";
 
-    image.style.display = "none";
+    dice_1.style.display = "none";
+    dice_2.style.display = "none";
 
     document.querySelector("#name-0").textContent = "Player-1";
     document.querySelector("#name-1").textContent = "Player-2";
@@ -114,6 +124,6 @@ function nextPlayer() {
     document.querySelector(".player-0-panel").classList.toggle("active");
     document.querySelector(".player-1-panel").classList.toggle("active");
 
-    // hide the image
-    // image.style.display = "none";
+    // hide the dice-1
+    // dice-1.style.display = "none";
 }
