a335fd11d8855c7a7b8079cc7b9dc98f950ab1ea
Ketul Makwana <ketulmakwana09@gmail.com>
2019-03-06 13:25:45 +0530

    Added JS for the PIG GAME

---
+++ pigGame.js
@@ -0,0 +1,109 @@
+var rollDice = document.querySelector(".btn-roll");
+var holdDice = document.querySelector(".btn-hold");
+var newGame = document.querySelector(".btn-new");
+var image = document.querySelector(".dice");
+
+var diceNum, scores, currentScore, activePlayer, gamePlaying;
+
+init();
+
+// roll dice Event
+rollDice.addEventListener("click", function() {
+    if (gamePlaying) {
+        // 1. Generate a random number and save it in a variable called "diceNum".
+        generateRandomNum();
+
+        // 2. If diceNum is not 1 then add the diceNum to currentScore otherwise go to next player.
+        if (diceNum !== 1) {
+            // Display the image
+            image.style.display = "block";
+
+            // Display the image corresponding to diceNum
+            image.src = "dice-" + diceNum + ".png";
+
+            //add diceNum to currentScore.
+            currentScore += diceNum;
+
+            // Display the currentScore.
+            document.querySelector("#current-" + activePlayer).textContent = currentScore;
+        } else {
+            nextPlayer();
+        }
+    }
+});
+
+
+// hold the score Event
+holdDice.addEventListener("click", function() {
+    if (gamePlaying) {
+        // add currentScore to Global Score
+        scores[activePlayer] += currentScore;
+
+        // update UI.
+        document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
+        // Chack if player won the game.
+        if (scores[activePlayer] >= 10) {
+            document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+            document.querySelector(".dice").style.display = "none";
+            document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+            gamePlaying = false;
+        } else {
+            nextPlayer();
+        }
+    }
+});
+
+
+// newGame Event
+newGame.addEventListener("click", init)
+
+//generate a random number and save it into a "diceNum" variable;
+function generateRandomNum() {
+    diceNum = Math.floor(Math.random() * 6 + 1);
+}
+
+// game initializer function
+function init() {
+    scores = [0, 0];
+    activePlayer = 0;
+    currentScore = 0;
+    gamePlaying = true;
+
+    document.getElementById("score-0").textContent = "0";
+    document.getElementById("score-1").textContent = "0";
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    image.style.display = "none";
+
+    document.querySelector("#name-0").textContent = "Player-1";
+    document.querySelector("#name-1").textContent = "Player-2";
+
+    document.querySelector(".player-0-panel").classList.remove("winner");
+    document.querySelector(".player-1-panel").classList.remove("winner");
+    document.querySelector(".player-0-panel").classList.remove("active");
+    document.querySelector(".player-1-panel").classList.remove("active");
+    document.querySelector(".player-0-panel").classList.add("active");
+
+}
+
+// function to change the player
+function nextPlayer() {
+    //next player's turn
+    // change the activePlayer variable
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+    //Next player's score starts with zero
+    currentScore = 0;
+
+    // Make currentScore of both the player's zero.
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    // toggle active to the corresponding active player's container.
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    // hide the image
+    image.style.display = "none";
+}
